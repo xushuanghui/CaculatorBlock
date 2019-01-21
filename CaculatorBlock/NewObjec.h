8c4326b93e6e29a0e4e5ea986d5e2056dea1fd65
maoxuewang <maoxuewang@2dfire.com>
2019-01-21 14:53:17 +0800

    new object

---
+++ CaculatorBlock/NewObjec.h
@@ -0,0 +1,17 @@
+//
+//  NewObjec.h
+//  CaculatorBlock
+//
+//  Created by shuanghui on 2019/1/21.
+//  Copyright © 2019 shuanghui xu. All rights reserved.
+//1.1
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NewObjec : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CaculatorBlock/NewObjec.m
@@ -0,0 +1,13 @@
+//
+//  NewObjec.m
+//  CaculatorBlock
+//
+//  Created by shuanghui on 2019/1/21.
+//  Copyright © 2019 shuanghui xu. All rights reserved.
+//
+
+#import "NewObjec.h"
+
+@implementation NewObjec
+
+@end
