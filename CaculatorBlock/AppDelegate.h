a206c4e099cae50e1aec87a483bb955a57c43c23
maoxuewang <maoxuewang@2dfire.com>
2019-04-04 16:16:51 +0800

    Revert "Merge branch 'feature/calculatorFour' into feature/caculatorTwo"
    This reverts commit 7a049bb618b3a5525f6674fa760853cae09b32f6, reversing
    changes made to 0af7b4c436bd0d181ef8ab12644284c8c8ea4189.

--- CaculatorBlock/AppDelegate.h
+++ CaculatorBlock/AppDelegate.h
@@ -3,7 +3,6 @@
 //  CaculatorBlock
 //  Created by shuanghui xu on 2017/7/26.
 //  Copyright © 2017年 shuanghui xu. All rights reserved.
-//1.2
 
 #import <UIKit/UIKit.h>
 
--- CaculatorBlock/Caculator.m
+++ CaculatorBlock/Caculator.m
@@ -5,7 +5,6 @@
 //  Created by shuanghui xu on 2017/7/26.
 //  Copyright © 2017年 shuanghui xu. All rights reserved.
 //777
-//99
 
 #import "Caculator.h"
 
